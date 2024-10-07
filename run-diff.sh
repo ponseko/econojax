c020adcf0bda44df76cfdf5fbf581bb84b75980e
Koen Ponse <k.ponse@liacs.leidenuniv.nl>
2024-10-07 18:54:59 +0200

    change back to some usual params and dump logs

--- run-diff.sh
+++ run-diff.sh
@@ -1,8 +1,8 @@
 for seed in {2..2} 
 do for num_resources in {6..10..2} 
 do
-    python train.py -s $seed -ps $seed -t 10000000 -a 40 -e 6 -w -wg Rrun-$num_resources -r $num_resources --trade_prices 3 6 9 --craft_diff_resources_required $(( (num_resources / 2)));
-    python train.py -s $seed -ps $seed -t 10000000 -a 40 -e 6 -w -wg Rrun-$num_resources -r $num_resources -i --trade_prices 3 6 9 --craft_diff_resources_required $(( (num_resources / 2)));
-    python train.py -s $seed -ps $seed -t 10000000 -a 40 -e 6 -w -wg Rrun-$num_resources -r $num_resources --insert_agent_ids True --trade_prices 3 6 9 --craft_diff_resources_required $(( (num_resources / 2)));
+    python train.py -s $seed -ps $seed -t 10000000 -a 40 -e 6 -w -wg resource-$num_resources -r $num_resources --trade_prices 3 6 9 --craft_diff_resources_required $(( (num_resources / 2)));
+    python train.py -s $seed -ps $seed -t 10000000 -a 40 -e 6 -w -wg resource-$num_resources -r $num_resources -i --trade_prices 3 6 9 --craft_diff_resources_required $(( (num_resources / 2)));
+    python train.py -s $seed -ps $seed -t 10000000 -a 40 -e 6 -w -wg resource-$num_resources -r $num_resources --insert_agent_ids True --trade_prices 3 6 9 --craft_diff_resources_required $(( (num_resources / 2)));
 done
 done
