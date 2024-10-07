027a2cfa1702f6ce24c9d63ecff7413ca572fff8
Koen Ponse <k.ponse@liacs.leidenuniv.nl>
2024-10-08 00:30:12 +0200

    resource run files

--- run-diff.sh
+++ run-diff.sh
@@ -1,8 +1,8 @@
-for seed in {2..2} 
-do for num_resources in {6..10..2} 
+for seed in {1..3} 
+do for num_resources in {2..12..2} 
 do
-    python train.py -s $seed -ps $seed -t 10000000 -a 40 -e 6 -w -wg resource-$num_resources -r $num_resources --trade_prices 3 6 9 --craft_diff_resources_required $(( (num_resources / 2)));
-    python train.py -s $seed -ps $seed -t 10000000 -a 40 -e 6 -w -wg resource-$num_resources -r $num_resources -i --trade_prices 3 6 9 --craft_diff_resources_required $(( (num_resources / 2)));
-    python train.py -s $seed -ps $seed -t 10000000 -a 40 -e 6 -w -wg resource-$num_resources -r $num_resources --insert_agent_ids True --trade_prices 3 6 9 --craft_diff_resources_required $(( (num_resources / 2)));
+    python train.py -s $seed -ps $seed -t 10000000 -a 40 -e 6 -w -wg $num_resources -r $num_resources --trade_prices 3 6 9 --craft_diff_resources_required 0;
+    python train.py -s $seed -ps $seed -t 10000000 -a 40 -e 6 -w -wg $num_resources -r $num_resources -i --trade_prices 3 6 9 --craft_diff_resources_required 0;
+    python train.py -s $seed -ps $seed -t 10000000 -a 40 -e 6 -w -wg $num_resources -r $num_resources --insert_agent_ids True --trade_prices 3 6 9 --craft_diff_resources_required 0;
 done
 done
